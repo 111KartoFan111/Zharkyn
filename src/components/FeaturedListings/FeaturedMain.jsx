cbf1241b5d913b5aa1e08f8ada5059bc58ee3acd
111KartoFan111 <zarkynismagulov@gmail.com>
2025-03-28 05:35:56 +0500

    Иду Спать

---
+++ src/components/FeaturedListings/FeaturedMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FeaturedMain.css'
+
+const FeaturedMain = () => {
+  return (
+    <div className='FMain'></div>
+  )
+}
+
+export default FeaturedMain
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,16 @@ import Header from './Header/Header'
 import Filter from './FilterBlock/Filter'
 import CarType from './CarType/CarType'
 import CarBrand from './CarBrand/CarBrand'
+import FeaturedMain from './FeaturedListings/FeaturedMain'
 
 const Main = () => {
   return (
     <div className='main'>
-        <Header />
-        <Filter />
-        <CarType />
-        <CarBrand />
+      <Header />
+      <Filter />
+      <CarType />
+      <CarBrand />
+      <FeaturedMain />
     </div>
   )
 }
